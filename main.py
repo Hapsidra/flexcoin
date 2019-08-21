7e4310d5cd9e6580ee3f6b434fd5bb7ac7957655
Gennady <dobrets@gmail.com>
2019-08-21 15:43:53 +0300

    add files

---
+++ main.py
@@ -0,0 +1,8 @@
+import cryptography
+import json
+import flask
+print("hello")
+print(cryptography.division);
+print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
+
+print(flask.__version__);
