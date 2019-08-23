81c6a85cac5777a674aed63f9d65efc8e46661b4
Max <hapsidra@gmail.com>
2019-08-23 13:18:56 +0300

    first version

--- main.py
+++ main.py
@@ -1,8 +1,84 @@
-import cryptography
-import json
-import flask
-print("hello")
-print(cryptography.division);
-print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
+from hashlib import sha256
+from cryptography.hazmat.primitives.hashes import SHA256
+from flask import Flask, request as req
 
-print(flask.__version__);
+
+class Transaction:
+    def __init__(self, sender, to, value, nonce):
+        self.sender = sender
+        self.to = to
+        self.value = value
+        self.nonce = nonce
+
+
+class Block:
+    def __init__(self, transactions, nonce=0):
+        self.transactions = transactions
+        self.nonce = nonce
+
+    def mine(self):
+        print(type(self.__dict__))
+
+
+class State:
+    def __init__(self, balance, nonce):
+        self.balance = balance
+        self.nonce = nonce
+
+
+transactions_pool: [Transaction] = []
+
+
+def is_valid_transaction(transaction: Transaction) -> bool:
+    sender_state = get_state(transaction.sender)
+    if sender_state.nonce <= transaction.nonce:
+        return False
+    if sender_state.balance < transaction.value:
+        return False
+    return True
+
+
+def add_transaction(transaction):
+    if is_valid_transaction(transaction):
+        transactions_pool.append(transaction)
+
+
+
+def get_all_transactions() -> [Transaction]:
+    return transactions_pool
+
+
+def get_state(address):
+    transactions = get_all_transactions()
+    nonce = 0
+    if len(transactions) > 0:
+        nonce = transactions[len(transactions) - 1].nonce
+    balance = 0
+    for transaction in transactions:
+        if transaction.from_ == address:
+            balance -= transaction.value
+        elif transaction.to == address:
+            balance += transaction.value
+    return State(balance, nonce)
+
+
+def create_server():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World'
+
+    @app.route('/new_transaction', methods=['POST'])
+    def new_transaction():
+        form = req.form
+        value = int(form['value'])
+        nonce = int(form['nonce'])
+        transaction = Transaction(form['sender'], form['to'], value, nonce)
+        add_transaction(transaction)
+        return 'ok'
+
+    return app
+
+
+create_server().run()
